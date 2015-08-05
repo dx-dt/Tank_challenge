fa4a787e4a2a3872afd14c0d939aa797d73cdc66
dx-dt <dx-dt@users.noreply.github.com>
2015-08-05 21:43:12 +0200

    Added Fuel class and unit tests for Fuel class

--- solution.py
+++ solution.py
@@ -3,6 +3,30 @@ testing = False
 import api
 import unittest
 
+class Fuel:
+    """
+    Class for handling calculations related to the tank's fuel.
+    """
+    def __init__(self):
+        """
+        Method that initialises Fuel class.
+        Declares variables old_fuel and current_fuel.
+        """
+        self.old_fuel = None
+        self.current_fuel = None
+    def update(self):
+        """
+        Method that updates the fuel levels each tick.
+        """
+        self.old_fuel = self.current_fuel
+        self.current_fuel = api.current_fuel()
+
+class testFuel(unittest.TestCase):
+    """
+    Unit test for Fuel class.
+    Placeholder for now.
+    """
+
 class Tank:
     """
     Class for the object that represents the tank that
@@ -13,12 +37,12 @@ class Tank:
         Method that initialises Tank class.
         Placeholder for now.
         """
-        pass
+        self.fuel = Fuel()
     def update(self):
         """
         Updates everyting associated with the tank each server tick.
         """
-        pass
+        self.fuel.update()
 
 class TestTank(unittest.TestCase):
     """
@@ -43,4 +67,5 @@ class Solution:
         """
         Method that is run by the server each tick.
         """
+        print(self.tank.fuel.current_fuel)
         self.tank.update()
