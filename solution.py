fc49c672e96e8e71ae483672b4b6caf472dc4f61
dx-dt <dx-dt@users.noreply.github.com>
2015-08-05 21:16:46 +0200

    Added Tank class and unit test for Tank class

--- solution.py
+++ solution.py
@@ -3,6 +3,24 @@ testing = False
 import api
 import unittest
 
+class Tank:
+    """
+    Class for the object that represents the tank that
+    is manouvered in the challenge.
+    """
+    def __init__(self):
+        """
+        Method that initialises Tank class.
+        Placeholder for now.
+        """
+        pass
+
+class TestTank(unittest.TestCase):
+    """
+    Unit tests for Tank class.
+    Placeholder for now.
+    """
+
 class Solution:
     """
     Class given by challenge. Entrence point.
@@ -14,7 +32,7 @@ class Solution:
         """
         if testing:
             unittest.main("solution")
-
+        self.tank = Tank()
     def update(self):
         """
         Method that is run by the server each tick.
