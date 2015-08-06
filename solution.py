51eee6acdf95c9a4c4bfd1db46f4c1139d8b0f5c
dx-dt <dx-dt@users.noreply.github.com>
2015-08-06 15:12:14 +0200

    Added damage method to Tank class, and unit test for said method

--- solution.py
+++ solution.py
@@ -33,8 +33,7 @@ class Fuel:
 class TestFuel(unittest.TestCase):
     """
     Unit test for Fuel class.
-    Tests delta method.
-    Tests update method.
+    Tests delta and update methods.
     """
     def testDelta(self):
         """
@@ -68,6 +67,15 @@ class Tank:
         Instantiates Fuel class.
         """
         self.fuel = Fuel()
+    def damage(self):
+        """
+        Method that returns bool value, true if damage has been taken since
+        last hit, false otherwise.
+        """
+        if self.fuel.delta() > 50:
+            return True
+        else:
+            return False
     def update(self):
         """
         Updates everyting associated with the tank each server tick.
@@ -77,8 +85,20 @@ class Tank:
 class TestTank(unittest.TestCase):
     """
     Unit tests for Tank class.
-    Placeholder for now.
+    Tests damage method.
     """
+    def testDamage(self):
+        """
+        Unit test that asserts damage method is reporting correctly.
+        """
+        mock = unittest.mock.MagicMock(return_value = 5)
+        with unittest.mock.patch('solution.Fuel.delta', mock):
+            self.tank = Tank()
+            self.assertFalse(self.tank.damage())
+        mock = unittest.mock.MagicMock(return_value = 55)
+        with unittest.mock.patch('solution.Fuel.delta', mock):
+            self.tank = Tank()
+            self.assertTrue(self.tank.damage())
 
 class Solution:
     """
